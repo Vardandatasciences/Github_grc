1a2131ce55bd3bdc4549343601c38d2d88e5b6ad
Vardandatasciences <vdatasciences@gmail.com>
2025-05-06 16:42:14 +0530

    Update test_app.py

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,7 +1,7 @@
 def happy_birthday(Suresh):
     print(f"Happy Birthday, {Suresh}!\nWishing you a day filled with love, laughter, and joy!")
 
-# Example of how you would test this in a unit test1
+# Example of how you would test this in a unit test
 def test_happy_birthday():
     suresh = "Suresh"
     happy_birthday(Suresh)
